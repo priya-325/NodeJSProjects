6fc0552f944aaf3a11bfd54ad93a2cb31a2770e6
Priyanka <priyanka.e.03.12@gmail.com>
2026-02-05 16:34:23 +0000

    added new project

--- frontend.js
+++
@@ -1,9 +0,0 @@
-/**** front end code****/
-const response = await fetch("https://example.org/post", {
-  method: "POST",
-  body: JSON.stringify({ username: "tom_1" }),
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-/**** ends *****/
--- utils/getDataByPathParams.js
+++
@@ -1,7 +0,0 @@
-export const getDataByPathParams = (data, locationType, locationName) => {
-  return data.filter((destination) => {
-    return (
-      destination[locationType].toLowerCase() === locationName.toLowerCase()
-    );
-  });
-};
